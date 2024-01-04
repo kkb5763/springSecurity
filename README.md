14efa5d61ac766f20b1e7d7f12a1c25f924d47c1
kkb57 <kkb5763@gmail.com>
2024-01-05 00:23:22 +0900

    11

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # springSecurity
 스프링 시큐리티
+
+사용 기술 : jpa, mybatis 병행
+
+swagger 접속 경로
+http://localhost:8080/swagger-ui/index.html
+
---
+++ src/main/java/com/kbkang/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.kbkang.config;
+
+import com.kbkang.config.auth.PrincipalDetailsService;
+import com.kbkang.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // IoC 빈(bean)을 등록
+public class SecurityConfig {
+
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
+//	@Autowired
+//	private PrincipalDetailsService principalDetailsService;
+
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/user/**").authenticated()
+				// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
+				// hasRole('ROLE_USER')")
+				// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
+				// hasRole('ROLE_USER')")
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/loginProc")
+				.defaultSuccessUrl("/")
+				.and()
+				.oauth2Login()
+				.loginPage("/login")
+				.userInfoEndpoint()
+				//.userService(principalDetailsService);
+				.userService(principalOauth2UserService);
+
+		return http.build();
+	}
+}
